052290442e3e617853d3615b51395854e1b95882
filipjt88 <filip88ks@gmail.com>
2024-01-30 15:13:55 +0100

    save

---
+++ main.js
@@ -0,0 +1,31 @@
+const items = [...document.querySelectorAll("h1")];
+// console.log(items);
+
+const updateCount = (el) => {
+    const value = parseInt(el.dataset.value);
+    const increment = Math.ceil(value / 1000);
+    let initialValue = 0;
+
+    const increaseCount = setInterval(() => {
+        initialValue+= increment;
+
+        if(initialValue > value) {
+            el.textContent = `${value}+`;
+            clearInterval(increaseCount);
+            return;
+        }
+
+        el.textContent = `${initialValue} +`;
+    },10);
+    console.log(increaseCount);
+    
+}
+
+items.forEach((item) => {
+    updateCount(item);
+});
+
+
+
+
+
